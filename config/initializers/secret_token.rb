61839cabd8912a10ef61b3ec14dbf5d53283632e
Berlimioz <berlimioz@berlimioz-N140.(none)>
2012-09-13 16:41:44 +0200

    First commit for easy_plan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyPlan::Application.config.secret_token = '9258115ab830ebfeefd9faf0be3df4f851ead6477b3228c0fd26996e543101666ba1847a3356d9a21d90c7468ab9e738691fa3e464e7bd87dd518a8c5190d67a'
